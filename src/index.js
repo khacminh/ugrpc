a228ee0fecb49a5e50cc93f31b67e480b17c5d0d
Minh Ho <minhho@itrvn.com>
2021-05-31 22:30:01 +0700

    export grpc status

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+const grpc = require('@grpc/grpc-js');
 const Server = require('./server');
 const Client = require('./client');
 
 module.exports = {
   Server,
   Client,
+  grpcStatus: grpc.status,
 };
